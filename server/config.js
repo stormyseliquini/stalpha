fdeedb213c8b1a94e31718c5337bf4faec4f0697
stormy seliquini <stormyseliquini3of3@gmail.com>
2017-04-23 18:52:49 -0700

    for serve

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 var PORT = proccess.env.PORT || 3000;
 module.exports = {
   "restApiRoot": "/api",
-  "host": "Public DNS: ec2-54-186-136-39.us-west-2.compute.amazonaws.com",
+  "host": "ec2-54-186-136-39.us-west-2.compute.amazonaws.com",
   "port": PORT,
   "remoting": {
     "context": false,
